e5db45e3f89acd0c26501f344d3ea24ae0c663ed
onfranciis <onukwuf@gmail.com>
2024-04-03 05:16:12 +0100

    fix: update response format

--- src/controllers/get_all_url.rs
+++ src/controllers/get_all_url.rs
@@ -2,6 +2,8 @@ use rocket::serde::json::Json;
 use serde::Serialize;
 use uuid::Uuid;
 
+use crate::catchers::catchers::IBaseResponse;
+
 #[derive(Serialize)] // Derive Serialize for IURL
 pub struct IURL {
     id: String,
@@ -10,13 +12,16 @@ pub struct IURL {
 }
 
 #[get("/url")]
-pub fn index() -> Json<IURL> {
+pub fn index() -> Json<IBaseResponse<self::IURL>> {
     let id: String = Uuid::new_v4().to_string();
 
-    let formatted_data: IURL = IURL {
-        id,
-        url: String::from("www.onfranciis.dev"),
-        short: String::from("odev"),
+    let formatted_data: IBaseResponse<self::IURL> = IBaseResponse::<self::IURL> {
+        err: None,
+        result: Some(self::IURL {
+            id,
+            url: String::from("www.onfranciis.dev"),
+            short: String::from("odev"),
+        }),
     };
 
     Json(formatted_data)
