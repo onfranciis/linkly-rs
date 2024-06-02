973450a447405acabe0a34fcc262c0af084f927e
onfranciis <onukwuf@gmail.com>
2024-06-02 13:46:53 +0100

    feat: create add_new_url controller

--- src/controllers/add_new_url.rs
+++ src/controllers/add_new_url.rs
@@ -1,18 +1,19 @@
+use crate::{
+    catchers::catchers::IBaseResponse,
+    util::{
+        other::{instantiate_table_query, IURL},
+        response::{
+            connection_error, create_table_error, insert_conflict, insert_failure,
+            invalid_form_data, response_success,
+        },
+        shortest::get_shortest_value,
+    },
+};
 use chrono::{FixedOffset, Utc};
-use mongodb::{Client, Collection};
+use regex::Regex;
 use rocket::{form::Form, http::Status, serde::json::Json};
-use serde::Serialize;
+use sqlx::postgres::PgPoolOptions;
 use uuid::Uuid;
-
-use crate::{catchers::catchers::IBaseResponse, util::shortest::get_shortest_value};
-
-#[derive(Serialize)]
-pub struct IURL {
-    id: String,
-    url: String,
-    short: String,
-    date: String,
-}
 
 #[derive(FromForm)]
 pub struct IBody {
@@ -23,78 +24,108 @@ pub struct IBody {
 pub async fn index(
     body: Option<Form<IBody>>,
 ) -> Result<(Status, Json<IBaseResponse<IURL>>), (Status, Json<IBaseResponse>)> {
-    let invalid_form_data: IBaseResponse = IBaseResponse {
-        err: Some(String::from("Invalid form format!")),
-        result: None,
-    };
-
-    let connection_error: IBaseResponse = IBaseResponse {
-        err: Some(String::from(
-            "Invalid MongoDB connection string! Update your .env",
-        )),
-        result: None,
-    };
-
-    let mongodb_url = match std::env::var("MONGODB_URL") {
+    let pg_url = match std::env::var("DATABASE_URL") {
         Ok(url) => url,
         Err(_err) => {
-            return Err((Status::BadRequest, Json(connection_error)));
+            return Err((Status::BadRequest, Json(connection_error())));
         }
     };
 
-    let client = match Client::with_uri_str(mongodb_url).await {
-        Ok(client) => client,
+    let data = match body {
+        None => return Err((Status::BadRequest, Json(invalid_form_data()))),
+        Some(data) => data,
+    };
+
+    let pool = match PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&pg_url)
+        .await
+    {
+        Ok(data) => data,
         Err(err) => {
-            println!("Error creating MongoDB client: {:?}", err);
-            return Err((Status::BadRequest, Json(connection_error)));
+            print!("{:?}", err);
+            return Err((Status::BadRequest, Json(connection_error())));
         }
     };
 
-    let url_collection: Collection<IURL> = client.database("linkly").collection("url");
-
-    async fn proceed(
-        data: Form<IBody>,
-        invalid_form_data: IBaseResponse,
-        url_collection: Collection<IURL>,
-    ) -> Result<(Status, Json<IBaseResponse<IURL>>), (Status, Json<IBaseResponse>)> {
-        let url: &String = &data.url;
-
-        // Throw 400 if url attribute of body object does not exists
-        if url.trim().is_empty() {
-            return Err((Status::BadRequest, Json(invalid_form_data.clone())));
+    // Create a table
+    match sqlx::query(&instantiate_table_query()).execute(&pool).await {
+        Ok(_data) => (),
+        Err(_err) => {
+            return Err((Status::BadRequest, Json(create_table_error())));
         }
+    };
 
-        // Create uuid, split it get the shortest word to use as short version
-        let id: String = String::from(Uuid::new_v4());
-        let uuid_array: Vec<&str> = id.split("-").collect::<Vec<&str>>();
-        let short: String = get_shortest_value(uuid_array).to_string();
-
-        // Add offset to make it GMT+1 i.e UTC+1
-        let offset = FixedOffset::east_opt(1 * 60 * 60).unwrap();
-        let curr_time = Utc::now()
-            .with_timezone(&offset)
-            .format("%d %b-%Y %H:%M:%S %P %z");
-        let doc = IURL {
-            id: id.to_string(),
-            url: String::from(url),
-            short,
-            date: curr_time.to_string(),
-        };
-
-        url_collection.insert_one(&doc, None).await.ok();
-
-        let response = IBaseResponse::<IURL> {
-            err: None,
-            result: Some(doc),
-        };
-
-        return Ok((Status::Accepted, Json(response)));
+    let regexp = Regex::new(r"^https?://").unwrap();
+    let mut url = String::from(&data.url.clone());
+    if !regexp.is_match(&url) {
+        url = format!("http://{}", &url);
     }
 
-    match body {
-        None => return Err((Status::BadRequest, Json(invalid_form_data.clone()))),
-        Some(data) => {
-            return proceed(data, invalid_form_data, url_collection).await;
-        }
+    // Throw 400 if url attribute of body object does not exists
+    if url.trim().is_empty() {
+        return Err((Status::BadRequest, Json(invalid_form_data())));
     }
+
+    // Check if it already exists
+    match sqlx::query_as!(
+        IURL,
+        r#"
+        SELECT * FROM url
+        WHERE long = $1
+        "#,
+        url
+    )
+    .fetch_all(&pool)
+    .await
+    {
+        Ok(data) => {
+            print!("{:?}", data);
+            if data.len() > 0 {
+                return Err((Status::Conflict, Json(insert_conflict(data[0].clone()))));
+            }
+        }
+        Err(_err) => (),
+    };
+
+    // Create uuid, split it and get the shortest word to use as the url short version
+    let id: String = String::from(Uuid::new_v4());
+    let uuid_array: Vec<&str> = id.split("-").collect::<Vec<&str>>();
+    let short: String = get_shortest_value(uuid_array).to_string();
+
+    // Add offset to make it GMT+1 i.e UTC+1
+    let offset = FixedOffset::east_opt(1 * 60 * 60).unwrap();
+    let curr_time = Utc::now()
+        .with_timezone(&offset)
+        .format("%d %b-%Y %H:%M:%S%P %z")
+        .to_string();
+
+    // Insert the url
+    let rows = match sqlx::query_as!(
+        IURL,
+        r#"
+        INSERT INTO url (short, long, date) 
+        values ($1, $2, $3)
+        RETURNING *
+        "#,
+        short,
+        url,
+        curr_time
+    )
+    .fetch_all(&pool)
+    .await
+    {
+        Ok(data) => {
+            print!("{:?}", data);
+            data[0].clone()
+        }
+        Err(_err) => {
+            return Err((Status::BadRequest, Json(insert_failure())));
+        }
+    };
+
+    Ok((
+        Status::Accepted,
+        Json::<IBaseResponse<IURL>>(response_success(rows.clone())),
+    ))
 }
