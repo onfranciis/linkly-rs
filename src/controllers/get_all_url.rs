063396cd15300abad4687f7966255071cb24f996
onfranciis <onukwuf@gmail.com>
2024-06-02 13:47:12 +0100

    feat: create get_all_url controller

--- src/controllers/get_all_url.rs
+++ src/controllers/get_all_url.rs
@@ -1,68 +1,62 @@
-use std::option::IntoIter;
-
+use crate::{
+    catchers::catchers::IBaseResponse,
+    util::{
+        other::{instantiate_table_query, IURL},
+        response::{connection_error, create_table_error, get_query_failure, response_success},
+    },
+};
 use rocket::{http::Status, serde::json::Json};
-use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgPoolOptions;
 
-use crate::catchers::catchers::IBaseResponse;
-use mongodb::{bson::doc, Client, Collection, Cursor};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct IURL {
-    id: String,
-    url: String,
-    short: String,
-    date: Option<String>,
-}
 #[get("/url")]
-pub async fn index() -> Result<(Status, Json<IBaseResponse>), (Status, Json<IBaseResponse>)> {
-    let connection_error: IBaseResponse = IBaseResponse {
-        err: Some(String::from(
-            "Invalid MongoDB connection string! Update your .env",
-        )),
-        result: None,
-    };
-
-    let mongodb_url = match std::env::var("MONGODB_URL") {
+pub async fn index(
+) -> Result<(Status, Json<IBaseResponse<Vec<IURL>>>), (Status, Json<IBaseResponse>)> {
+    let pg_url = match std::env::var("DATABASE_URL") {
         Ok(url) => url,
         Err(_err) => {
-            return Err((Status::BadRequest, Json(connection_error)));
+            return Err((Status::BadRequest, Json(connection_error())));
         }
     };
 
-    let client = match Client::with_uri_str(mongodb_url).await {
-        Ok(client) => client,
+    let pool = match PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&pg_url)
+        .await
+    {
+        Ok(data) => data,
         Err(err) => {
-            println!("Error creating MongoDB client: {:?}", err);
-            return Err((Status::BadRequest, Json(connection_error)));
+            print!("{:?}", err);
+            return Err((Status::BadRequest, Json(connection_error())));
         }
     };
 
-    let url_collection: Collection<IURL> = client.database("linkly").collection("url");
+    // Create a table
+    match sqlx::query(&instantiate_table_query()).execute(&pool).await {
+        Ok(_data) => (),
+        Err(_err) => {
+            return Err((Status::BadRequest, Json(create_table_error())));
+        }
+    };
 
-    async fn proceed(
-        url_collection: Collection<IURL>,
-    ) -> Result<(Status, Json<IBaseResponse>), (Status, Json<IBaseResponse>)> {
-        let cursor = match url_collection.find(doc! {"url":"google.com"}, None).await {
-            Ok(result) => result,
-            Err(err) => {
-                print!("{:?}", (*err.kind));
-                return Err((
-                    Status::BadRequest,
-                    Json(IBaseResponse {
-                        err: Some(String::from("*err.kind")),
-                        result: None,
-                    }),
-                ));
-            }
-        };
+    // Query the url
+    let rows = match sqlx::query_as!(
+        IURL,
+        r#"
+        SELECT * 
+        FROM url
+        "#
+    )
+    .fetch_all(&pool)
+    .await
+    {
+        Ok(data) => data,
+        Err(_err) => {
+            return Err((Status::BadRequest, Json(get_query_failure())));
+        }
+    };
 
-        let pre_response: IBaseResponse = IBaseResponse {
-            err: None,
-            result: Some(String::from("Hi")),
-        };
-
-        return Ok((Status::Accepted, Json(pre_response)));
-    }
-
-    proceed(url_collection).await
+    Ok((
+        Status::Accepted,
+        Json::<IBaseResponse<Vec<IURL>>>(response_success(rows)),
+    ))
 }
