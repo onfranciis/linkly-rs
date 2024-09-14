9d72f6683a029b385111505299be98bc8b431e2a
onfranciis <onukwuf@gmail.com>
2024-09-14 16:15:09 +0100

    feat: instantiate redis

--- src/util/other.rs
+++ src/util/other.rs
@@ -1,4 +1,7 @@
+use chrono::{FixedOffset, Utc};
 use dotenv::dotenv;
+use redis::Commands;
+use rocket::serde::json::to_string;
 use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, Postgres};
 use std::env;
@@ -23,6 +26,17 @@ pub struct IURLWithoutID {
     pub long: String,
     pub short: String,
     pub date: String,
+}
+
+pub fn current_time() -> String {
+    // Add offset to make it GMT+1 i.e UTC+1
+    let offset = FixedOffset::east_opt(1 * 60 * 60).unwrap();
+    let curr_time = Utc::now()
+        .with_timezone(&offset)
+        .format("%d %b-%Y %H:%M:%S%P %z")
+        .to_string();
+
+    curr_time
 }
 
 pub async fn setup() -> (String, String, sqlx::Pool<Postgres>) {
@@ -55,6 +69,56 @@ pub async fn setup() -> (String, String, sqlx::Pool<Postgres>) {
     .execute(&pg_pool)
     .await
     .expect("Something went wrong while instantiating the database!");
+    setup_redis(&redis_url).await;
 
     (redis_url, pg_url, pg_pool)
+}
+
+pub async fn setup_redis(redis_url: &String) -> () {
+    let client = redis::Client::open(redis_url.as_str()).expect("Could not open redis connection!");
+    let mut conn = client
+        .get_connection()
+        .expect("Could not connect to Redis!");
+    let demo_key = "demo";
+    let demo_data = vec![IURLWithoutID {
+        date: current_time(),
+        short: String::from(demo_key),
+        long: String::from("https://github.com/onfranciis/linkly-rs"),
+    }];
+
+    // Check if urls key exist in redis
+    match conn.get::<&str, Option<Vec<String>>>("urls") {
+        Ok(None) => {
+            // If it doesn't exist? Create urls key and attatch demo data to it
+            match conn.set::<&str, String, String>("urls", to_string(&demo_data).unwrap()) {
+                Ok(_) => println!("Instantiated redis with {:?}", &demo_data),
+
+                Err(_) => {
+                    panic!(
+                        "Something went wrong while trying to add {:?} to redis",
+                        &demo_data
+                    );
+                }
+            }
+        }
+
+        Ok(Some(_)) => (),
+
+        Err(_) => panic!("Something went wrong while instantiating redis!"),
+    }
+
+    // Set demo key to match demo data
+    match conn.set::<&str, String, String>("demo", to_string(&demo_data[0]).unwrap()) {
+        Ok(_) => println!(
+            "Added {:?} to redis with key {:?}",
+            &demo_data[0], &demo_key
+        ),
+
+        Err(_) => {
+            panic!(
+                "Something went wrong while trying to add {:?} to key '{:?}' in redis!",
+                &demo_data, &demo_key
+            );
+        }
+    }
 }
